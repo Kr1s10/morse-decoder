79be446b33aa10160625459851ad5ad7d8437a1e
Kristen <kr1s10.khud@gmail.com>
2022-02-23 09:03:04 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const keys = Object.keys(MORSE_TABLE);
+    const arrOfLetters = [];
+    const morseLetters = [];
+    let result = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        arrOfLetters.push(expr.slice(i, i + 10));
+    }
+
+    arrOfLetters.map( letter => morseLetters.push(changeLetters(letter)));
+
+    for (const letter of morseLetters) {
+        if (letter.length === 0) result += ' ';
+
+        keys.map( key => key === letter ? result += MORSE_TABLE[key] : result += '');
+    }
+
+    return result;
+}
+
+function changeLetters (str) {
+    const arrOfSymbols = [];
+    const start = str.indexOf('1');
+  
+    if (start < 0) return '';
+  
+    for (let i = start; i < str.length; i += 2) {
+      const symbol = str.slice(i, i + 2);
+  
+      if (symbol === '10') arrOfSymbols.push('.');
+  
+      if (symbol === '11') arrOfSymbols.push('-');
+    }
+  
+    return arrOfSymbols.join('');
 }
 
 module.exports = {
